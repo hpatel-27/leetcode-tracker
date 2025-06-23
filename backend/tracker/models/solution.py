12be36981aa99c7e7ca042946f6fe4333efa1cce
Harsh Patel <harshpxv@gmail.com>
2025-06-22 22:48:01 -0400

    feat: str definition for solution

--- backend/tracker/models/solution.py
+++ backend/tracker/models/solution.py
@@ -12,3 +12,6 @@ class Solution(models.Model):
     drawing = models.ImageField(upload_to="drawings/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Solution for {self.problem.title} in {self.language}"
