4c942edb2a3c051031db12dd07b4163c82583677
Harsh Patel <harshpxv@gmail.com>
2025-06-26 23:01:59 -0400

    feat: add serializer imports to package declaration

---
+++ backend/tracker/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .problem_serializer import ProblemSerializer
+from .solution_serializer import SolutionSerializer
+from .tag_serializer import TagSerializer
+from .user_serializer import UserSerializer
